191e3a54de9e8e7b8ebe22853a6639d80e5edc47
uyendo <uyen.do@apps-cyclone.com>
2019-04-25 23:26:07 +0700

    update code done

--- src/scripts/public/index.js
+++ src/scripts/public/index.js
@@ -2,7 +2,7 @@ $('.slider1').not('.slick-initialized').slick({
     dots: false,
     arrows:false,
     infinite: false,
-    autoplay:true,
+    // autoplay:true,
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -11,12 +11,12 @@ $('.slider2').not('.slick-initialized').slick({
     dots: false,
     arrows:true,
     infinite: false,
-    autoplay:false,
+    autoplay:true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 1,
-    prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./assets/images/arrow-1.jpg' ></button>",
-    nextArrow: "<button type='button' class='slick-next pull-right'><img src='./assets/images/arrow-2.jpg'></button>",
+    prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./assets/images/arrow-1.png' ></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><img src='./assets/images/arrow-2.png'></button>",
     responsive: [
         {
           breakpoint: 1350,
@@ -53,11 +53,11 @@ $('.slider3').not('.slick-initialized').slick({
     arrows:true,
     infinite: false,
     speed: 300,
-    autoplay:true,
+    // autoplay:true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./assets/images/arrow-1.jpg' ></button>",
-    nextArrow: "<button type='button' class='slick-next pull-right'><img src='./assets/images/arrow-2.jpg'></button>",
+    prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./assets/images/arrow-1.png' ></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><img src='./assets/images/arrow-2.png'></button>",
     responsive: [
         {
           breakpoint: 1350,
@@ -98,8 +98,8 @@ $('.slider4').not('.slick-initialized').slick({
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./assets/images/arrow-1.jpg' ></button>",
-    nextArrow: "<button type='button' class='slick-next pull-right'><img src='./assets/images/arrow-2.jpg'></button>",
+    prevArrow: "<button type='button' class='slick-prev pull-left'><img src='./assets/images/arrow-1.png' ></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><img src='./assets/images/arrow-2.png'></button>",
     responsive: [
         {
           breakpoint: 1350,
@@ -123,3 +123,45 @@ $(document).click(function(e){
    
     $('.header__nav').removeClass('active')
 })
+
+
+$(document).ready(function() {
+
+  /* 1. Visualizing things on Hover - See next part for action on click */
+  $('#stars li').on('mouseover', function() {
+      var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+
+      // Now highlight all the stars that's not after the current hovered star
+      $(this).parent().children('li.star').each(function(e) {
+          if (e < onStar) {
+              $(this).addClass('hover');
+          } else {
+              $(this).removeClass('hover');
+          }
+      });
+
+  }).on('mouseout', function() {
+      $(this).parent().children('li.star').each(function(e) {
+          $(this).removeClass('hover');
+      });
+  });
+
+
+  /* 2. Action to perform on click */
+  $('#stars li').on('click', function() {
+      var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+      var stars = $(this).parent().children('li.star');
+
+      for (i = 0; i < stars.length; i++) {
+          $(stars[i]).removeClass('selected');
+      }
+
+      for (i = 0; i < onStar; i++) {
+          $(stars[i]).addClass('selected');
+      }
+
+  });
+
+
+});
+
